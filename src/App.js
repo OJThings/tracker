aad77f23a796fdc43a95a6739e7bf433668980e8
Jerome Dumagsang <netflixkenya13@gmail.com>
2022-08-31 10:40:03 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
 import IncomeForm from "./components/IncomeForm";
-import Header from './components/Header'
+import Header from "./components/Header";
+import IncomeList from "./components/IncomeList";
 
 function App() {
   const [income, setIncome] = useState([]);
 
   const [total, setTotal] = useState(0);
 
+  useEffect(() => {
+    let temp = 0;
+    for (let i = 0; i < income.length; i++) {
+      temp += parseInt(income[i].price);
+    }
 
+    setTotal(temp);
+  }, [income]);
   return (
-  
-    
     <div className="App">
-
-      <Header total={total}/>
-      <IncomeForm income={income} setIncome={setIncome}/>
-      {(income.length > 0) ? income[0].desc: ''}
+      <Header total={total} />
+      <IncomeForm income={income} setIncome={setIncome} />
+      {income.length > 0 ? income[0].desc : ""}
+      <IncomeList income={income} setIncome={setIncome} />
     </div>
-  
-  
-  
-    )
+  );
 }
 
 export default App;
